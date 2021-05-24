b56264009025cccba35a541fe9fccfa74ead4d0f
Richard Foster <richard@rpf.me>
2021-05-25 00:08:28 +0100

    Support lazy and async dependencies

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,4 +1,5 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks'
+import { dedent } from 'ts-dedent'
 import { DependencyProvider, useDependency, useHook } from './'
 
 const TEST_KEY_NAMES = ['random', 'whatever', Math.random().toString()]
@@ -52,4 +53,111 @@ test.each(TEST_KEY_NAMES)('useHook should throw an error if it is not set', key 
   const { result } = renderHook(() => useHook<() => number>(key), { wrapper })
 
   expect(result.error).toEqual(Error(`The dependency "${key}" is not set`))
+})
+
+describe('with lazy dependencies', () => {
+  test('useDependency should call the load function', async () => {
+    const random = Math.random()
+    const wrapper = createWrapper({
+      rng: {
+        load: () => random
+      },
+    })
+
+    const { result } = renderHook(() => useDependency<number>('rng'), { wrapper })
+
+    expect(result.current).toEqual(random)
+  })
+
+  test('useDependency should only call the dependencies which are used', async () => {
+    const loadDep1 = jest.fn(() => Math.random())
+    const loadDep2 = jest.fn(() => Math.random())
+
+    const wrapper = createWrapper({
+      one: { load: loadDep1 },
+      two: { load: loadDep2 },
+    })
+
+    renderHook(() => useDependency<number>('two'), { wrapper })
+
+    expect(loadDep1).not.toHaveBeenCalled()
+    expect(loadDep2).toHaveBeenCalled()
+  })
+})
+
+describe('with async dependencies', () => {
+  test('useDependency should return the default value of the async dependency at first', async () => {
+    const random1 = Math.random()
+    const random2 = Math.random()
+    const wrapper = createWrapper({
+      rng: {
+        load: async () => random2,
+        default: random1,
+      },
+    })
+
+    const { result, waitForNextUpdate } = renderHook(() => useDependency<number>('rng'), { wrapper })
+
+    expect(result.current).toEqual(random1)
+    await waitForNextUpdate()
+  })
+
+  test('useDependency should return the resolved value of the async dependency eventually', async () => {
+    const random1 = Math.random()
+    const random2 = Math.random()
+    const wrapper = createWrapper({
+      rng: {
+        load: async () => random2,
+        default: random1,
+      },
+    })
+
+    const { result, waitForNextUpdate } = renderHook(() => useDependency<number>('rng'), { wrapper })
+
+    await waitForNextUpdate()
+
+    expect(result.all).toEqual([random1, random2])
+  })
+
+  test('useDependency should default to a null value if no default is given', async () => {
+    const wrapper = createWrapper({
+      rng: {
+        load: async () => Math.random(),
+      },
+    })
+
+    const { result, waitForNextUpdate } = renderHook(() => useDependency<number>('rng'), { wrapper })
+
+    expect(result.current).toEqual(null)
+    await waitForNextUpdate()
+  })
+
+  test.each(TEST_KEY_NAMES)('useHook should throw an error if no default value is given', async key => {
+    const wrapper = createWrapper({
+      [key]: {
+        load: async () => () => Math.random(),
+      },
+    })
+
+    const { result } = renderHook(() => useHook(key), { wrapper })
+
+    expect(result.error).toEqual(new Error(dedent`
+      \`useHook\` was called with a dependency which has no \`default\` value.
+
+      This causes an error because it's generally easier to specify a default value
+      in the dependency configuration than it is to handle unexpected \`null\` values
+      in components.
+
+      To fix, specify a \`default\` value for the dependency. For example:
+
+      \`\`\`
+      const dependencies = {
+        ${key}: {
+          load: async () => import('./hooks/example'),
+          default: { loading: true },
+        }
+      }
+      \`\`\`
+    `))
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,70 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { dedent } from 'ts-dedent'
 
 type ArgumentTypes<F extends Function> = F extends (...args: infer A) => any ? A : never;
+
 export type DependencyMap = { [key: string]: any }
+export type DependencyConfig<T> = {
+  load: () => T | Promise<T>
+  default?: T
+}
 
 const context = createContext({} as DependencyMap)
 
 export const DependencyProvider = context.Provider
 
-export function useDependency<T>(name: string): T {
+export function useDependency<T>(name: string): T | null {
   const dependencies = useContext(context)
+  const [loaded, setLoaded] = useState(null as T | null)
 
-  if (name in dependencies) return dependencies[name] as T
+  const exists = name in dependencies
+  if (!exists) throw new Error(`The dependency "${name}" is not set`)
 
-  throw new Error(`The dependency "${name}" is not set`)
+  const dependency = dependencies[name] as T | DependencyConfig<T>
+  const resolved = dependency as T
+  const dynamic = dependency as DependencyConfig<T>
+  const isDynamic = typeof dependency === 'object'
+    && 'load' in dependency
+
+  useEffect(() => {
+    if (!isDynamic) return
+
+    const value = dynamic.load()
+    if (value instanceof Promise) {
+      value.then((result: T) => setLoaded(result))
+    } else {
+      setLoaded(value)
+    }
+  }, [dependency])
+
+  if (isDynamic) {
+    return loaded || dynamic.default || null
+  } else {
+    return resolved
+  }
 }
 
 export function useHook<T extends (...args: any) => any>(name: string, ...args: ArgumentTypes<T>): ReturnType<T> {
   const dependency = useDependency<T>(name)
+
+  if (dependency === null) throw new Error(dedent`
+    \`useHook\` was called with a dependency which has no \`default\` value.
+
+    This causes an error because it's generally easier to specify a default value
+    in the dependency configuration than it is to handle unexpected \`null\` values
+    in components.
+
+    To fix, specify a \`default\` value for the dependency. For example:
+
+    \`\`\`
+    const dependencies = {
+      ${name}: {
+        load: async () => import('./hooks/example'),
+        default: { loading: true },
+      }
+    }
+    \`\`\`
+  `)
+
   return dependency(...args)
 }
